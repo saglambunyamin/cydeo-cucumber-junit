dbcf1751381a1db77baa03180f2d2aabe4d4887c
Bunyamin <saglambunyamin@gmail.com>
2023-03-05 08:15:57 +0300

    Project Updated

--- src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
@@ -7,18 +7,15 @@ public class LoginStepDefinitions {
 
     @When("user enters librarian username")
     public void user_enters_librarian_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("user enters librarian password")
     public void user_enters_librarian_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 }
